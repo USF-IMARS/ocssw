776e86ce3d29c06eb5b6c68e317de7f19d951a6c
Tylar Murray <murray.tylar@gmail.com>
2018-05-21 22:38:25 +0000

    + submodules & setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+git submodule add -b v7.5 https://oceandata.sci.gsfc.nasa.gov/ocssw/bin-linux_64.git ./run/bin/linux_64
+git submodule add -b v7.5 https://oceandata.sci.gsfc.nasa.gov/ocssw/opt-linux_64.git ./run/opt/linux_64
+git submodule add -b v7.5 https://oceandata.sci.gsfc.nasa.gov/ocssw/common.git ./run/data/common
+git submodule add -b v7.5 https://oceandata.sci.gsfc.nasa.gov/ocssw/scripts.git ./run/scripts
